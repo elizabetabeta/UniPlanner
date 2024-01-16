882a52d631fcee4e544608e01cb1db20d77eb78f
ivanavas <ivanavasilj1@gmail.com>
2024-01-16 20:06:31 +0100

    Malo frontend second activity, register activity,main activity - dodatno

--- app/src/main/java/ba/sum/fpmoz/uniplanner/SecondActivity.java
+++ app/src/main/java/ba/sum/fpmoz/uniplanner/SecondActivity.java
@@ -2,7 +2,10 @@ package ba.sum.fpmoz.uniplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+
     }
 }
